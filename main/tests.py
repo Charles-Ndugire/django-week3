3a68b468184b92e3d09af3fcb061bd10328bebaa
chargeAhead <ndugirecharles@gmail.com>
2021-04-07 12:33:46 +0300

    Add user 1

--- main/tests.py
+++ main/tests.py
@@ -13,7 +13,7 @@ class ProjectTestClass(TestCase):
 
 class RatingTestClass(TestCase):
     def setUp(self):
-        self.user1 = User(username="kevin")
+        self.user1 = User(username="charles")
         self.user1.save()
         self.project1 = Project(title="master",description="master projo",link="https://master.com",image="master",user=self.user1)
         self.project1.save()
